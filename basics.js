ced6541eb0587da603d5d914e7daba2cdc0d9e88
Craje <crajeextremeyt.com>
2025-01-30 09:19:33 +0800

    second

--- basics.js
+++ basics.js
@@ -19,17 +19,25 @@ data.forEach((element) => {
 // let average = sum / arr1.length;
 // console.log(average);
 
-let human = {
-  //object
-  name: "mark",
-  gender: "male",
-  age: "31",
-  minor: false,
-  subjects: ["english", "math"],
-  grades: {
-    english: 90,
-    science: 95,
-    filipino: 85,
-  },
-};
-console.log(human.grades.science); // dictionary name.the title. the contents
+// let human = {
+//   //object
+//   name: "mark",
+//   gender: "male",
+//   age: "31",
+//   minor: false,
+//   subjects: ["english", "math"],
+//   grades: {
+//     english: 90,
+//     science: 95,
+//     filipino: 85,
+//   },
+// };
+// console.log(human.grades.science); // dictionary name.the title. the contents
+
+function addition() {
+  let num1 = 100;
+  let num2 = 50;
+  let sum = num1 + num2;
+  return sum;
+}
+console.log(addition());
